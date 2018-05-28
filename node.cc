5c65360a94508d9d1ab7bb2492419e64fcd1a88c
Nathaniel Wong <natmikewong@gmail.com>
2018-05-27 20:24:01 -0500

    sadf

--- node.cc
+++ node.cc
@@ -198,7 +198,7 @@ void Node::ProcessIncomingRoutingMessage(const RoutingMessage *m)
 {
   unsigned src = m->sender;
   std::vector<double> new_vec = m->dist_vec;
-  cerr << *this <<": Received message from "<<src<<":  "<<new_vec<<endl;
+  cerr << *this <<": Received message from "<<src<<endl;
 
   if (src != this->GetNumber())
   {
