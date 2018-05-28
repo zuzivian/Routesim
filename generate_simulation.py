2e570a6bc59b42eac8a9e7139bdfcd9cb51b68ce
Nathaniel Wong <natmikewong@gmail.com>
2018-05-28 00:09:15 -0500

    safd

--- generate_simulation.py
+++ generate_simulation.py
@@ -29,11 +29,11 @@ def generate_simulation(n, degree, time, filename):
             for j in range(int(math.floor(math.log(n,2)))):
                 offset = 1<<j
                 for neighbor in [i+offset, i-offset]:
-                    avail = true
+                    avail = 1
                     for (a, b, c) in links:
                         if (a==j and b==neighbor):
                             avail_degree -= 1
-                            avail = false
+                            avail = 0
                             break
                     if avail:
                         possible_neighbors.append(neighbor)
