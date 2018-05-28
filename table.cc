44113162f37a31e4e8710b1843f418d91ae2b093
Nathaniel Wong <natmikewong@gmail.com>
2018-05-28 18:05:48 -0500

    asdf

--- table.cc
+++ table.cc
@@ -31,7 +31,7 @@ Table::Table(const Table &rhs) :
 ostream & Table::Print(ostream &os) const
 {
   os << "\nnext hop\n";
-  map<unsigned, double>::iterator it;
+  map<unsigned, unsigned>::iterator it;
   for (it = next_hop.begin(); it != next_hop.end(); it++) {
     os << it->first << ": " << it->second << endl;
   }
